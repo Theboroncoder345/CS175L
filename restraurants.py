600dc78c830ae86f56c960d05eb23b0aaf6213d8
Theboroncoder345 <146225643+Theboroncoder345@users.noreply.github.com>
2024-02-08 12:50:45 -0500

    Add files via upload
    Arnav Vasa
    CS175L
    restaurants v2

--- restraurants.py
+++ restraurants.py
@@ -6,22 +6,30 @@ vegetarian = False
 vegan = False
 gluten_free = False
 
-response = input("Is anyone in your party a vegetarian?: ")
-if (response == 'yes'):
-    vegetarian = True
-response = input("Is anyone in your party a vegan?: ")
-if (response == 'yes'):
-    vegan = True
-response = input("Is anyone in your party gluten free?: ")
-if (response == 'yes'):
-    gluten_free = True
+keep_going = 'y'
+while keep_going == 'y':
+    vegetarian = False
+    vegan = False
+    gluten_free = False
+    
+    response = input("Is anyone in your party a vegetarian?: ")
+    if (response == 'yes'):
+        vegetarian = True
+    response = input("Is anyone in your party a vegan?: ")
+    if (response == 'yes'):
+        vegan = True
+    response = input("Is anyone in your party gluten free?: ")
+    if (response == 'yes'):
+        gluten_free = True
 
-print("Here are your restaurant choices:")
-if (not vegetarian and not vegan and not gluten_free):
-    print("Joe's Gourmet Burgers")
-if (not vegan and not gluten_free):
-    print("Mama's Fine Italian")
-if (not vegan):
-    print("Main Street Pizza")
-print("Corner Cafe")
-print("Chef's Kitchen")
+    print("Here are your restaurant choices:")
+    if (not vegetarian and not vegan and not gluten_free):
+        print("Joe's Gourmet Burgers")
+    if (not vegan and not gluten_free):
+        print("Mama's Fine Italian")
+    if (not vegan):
+        print("Main Street Pizza")
+    print("Corner Cafe")
+    print("Chef's Kitchen")
+    
+    keep_going = input('Do you want to do another search?("y" for Yes): ')
