eadcfca621d86cceee7a60d82a0089d521eb32db
Theboroncoder345 <146225643+Theboroncoder345@users.noreply.github.com>
2024-03-21 10:24:00 -0400

    Add files via upload
    v2

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -1,27 +1,46 @@
 #Arnav Vasa
 #CS175L
-#Average From Input File
+#Average From Input File v2
 
 #The program reads and displays the contents of the numbers.txt file.
 def main():
+
+    # call the function
+    avgerrorcheck()
+
+def avgerrorcheck():
     # Initializing total and index
     total = 0
     index = 0
-    
+
     # Open a file named numbers.txt file.
     infile = open('numbers.txt', 'r')
 
-    # Read the amount of integers from the file
+    # Reading each input
     for num in infile:
         number = num.split()
-        total += float(number[0])
-        index += 1
-        print('I read in', index, 'number(s) Current number is:', float(number[0]), 'Total is:', float(total))
+        integer = number[0]
+        check = False
 
-    #Display the average
+        # Check input's data type
+        for i in range(len(integer)):
+            if ord(integer[i]) >= 48 and ord(integer[i]) <= 57 or ord(integer[i]) == 45:
+                check = True
+
+        # Sum executes
+        if check == True:
+            index += 1
+            total += float(integer)
+            print('I read in', index, 'number(s) Current number is:', float(integer), 'Total is:', float(total))
+
+        # Skip executes
+        else:
+            print(f'Bad data: {integer} skipping')
+
+    # Calculate Average
     print(f'Average: {total/index}')
-    
-    #Close the file.
+
+    # Close the file.
     infile.close()
 
 # Call the main function.
